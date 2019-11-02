f0b12c5b0a40daf49e1d43b13f2fd90457b57b97
Hasan Akkas <hasanakkas@Hasans-MacBook-Pro.local>
2019-11-02 14:13:50 +0100

    last version

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,7 +1,8 @@
 import { createClient } from 'contentful';
 
+
 export default createClient({
-    space: '0mpzyie7ddmq',
-    accessToken: 'Q_6ofBKHfHE5xVcD5s_P6Us-3owe2mPl2UUP_XIYjiw'
+    space: process.env.REACT_APP_API_SPACE,
+    accessToken: process.env.REACT_APP_ACCESS_TOKEN
 });
 
